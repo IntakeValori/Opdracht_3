67a359eeffbc846756e368fbc60b29e39de66a16
Ico Bakker <IcoBakker@Valori.nl>
2021-02-25 11:51:32 +0100

    Refreshed dependencies and readme

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -2,14 +2,14 @@ package Runner;
 
 
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/featurefiles"
         ,glue={"stepdefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 
 public class TestRunner {
