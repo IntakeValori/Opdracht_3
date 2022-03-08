6343b5c2e22f8ecc6431d108451b61b3c7bf1388
laviniamicu78 <100972952+laviniamicu78@users.noreply.github.com>
2022-03-08 13:11:30 +0100

    Update TestRunner.java
    to create more / different test reports

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,9 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/featurefiles"
-        ,glue={"stepdefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        features = "src/test/resources/featurefiles", // The path of feature files
+        glue={"stepdefinitions"},  // The path of the step definition files
+        plugin = {"pretty", "html:target/cucumber-reports.html", "html:test-output", "json: json_output/cucumber.json", "junit:junit_xml/cucumber.xml"},
+        monochrome = true // Display the console output in a proper readable format
 )
 
 public class TestRunner {
