0f703848951a83b91b28b10dea6dea57e8566817
laviniamicu78 <100972952+laviniamicu78@users.noreply.github.com>
2022-03-08 12:45:13 +0100

    Update ContactUsPage.java
    update ContactUsPage for step (Then the contact us form is available) 
    add id en xpath for Contact page (contactformulier) https://www.valori.nl/neem-contact-op

--- src/main/java/pages/ContactUsPage.java
+++ src/main/java/pages/ContactUsPage.java
@@ -12,6 +12,14 @@ public class ContactUsPage {
     private int timeOut = 5;
 
     private By contactUsPageTitle = By.xpath("//h1[contains(text(),'Contact')]");
+    private By firstName = By.id("form-input-firstName");
+    private By lastName = By.id("form-input-lastName");
+    private By companyName = By.id ("form-input-companyName");
+    private By phone = By.id("form-input-homePhone");
+    private By email = By.id("form-input-email");
+    private By message = By.id("form-input-message");
+    private By sendButton = By.xpath("/html/body/div[2]/main/div/section/article/div[2]/form/fieldset/div[5]/button");
+
 
     public ContactUsPage(WebDriver driver){
         PageFactory.initElements(driver,this);
@@ -22,6 +30,47 @@ public class ContactUsPage {
         WebDriverWait wait = new WebDriverWait(driver,timeOut);
         wait.until(ExpectedConditions.presenceOfElementLocated(contactUsPageTitle));
         return driver.findElement(contactUsPageTitle).isDisplayed();
+    }
+
+    public Boolean isFirstnameVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(firstName));
+        return driver.findElement(firstName).isDisplayed();
+    }
+
+    public Boolean isLastnameVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(lastName));
+        return driver.findElement(lastName).isDisplayed();
+    }
+
+    public Boolean isCompanyVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(companyName));
+        return driver.findElement(companyName).isDisplayed();
+    }
+
+    public Boolean isPhoneVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(phone));
+        return driver.findElement(phone).isDisplayed();
+    }
+
+    public Boolean isEmailVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(email));
+        return driver.findElement(email).isDisplayed();
+    }
+
+    public Boolean isMessageVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(message));
+        return driver.findElement(message).isDisplayed();
+    }
+    public Boolean isSendVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,timeOut);
+        wait.until(ExpectedConditions.presenceOfElementLocated(sendButton));
+        return driver.findElement(sendButton).isDisplayed();
     }
 
 }
